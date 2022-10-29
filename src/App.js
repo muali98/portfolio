32379c00e652e08f2ab18e45c7af122dcad682cf
Ali-vspace <37alirisalpur@gmail.com>
2022-10-29 13:13:40 +0500

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Header } from "./MyComponents/Header"
+import { Main } from "./MyComponents/Main"
+import { Footer } from "./MyComponents/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ali, you did it
-          Now, you are re-testing ITTtttttT
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
