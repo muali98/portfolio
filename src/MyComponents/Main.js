e5e848ded47f1970e5b7c2c01083e2e8f989fcf8
Ali-vspace <37alirisalpur@gmail.com>
2022-12-21 19:57:13 +0500

    as

--- src/MyComponents/Main.js
+++ src/MyComponents/Main.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import Image1 from "./../Media/1.png"
 import Image2 from "./../Media/2.png"
-import Image3 from "./../Media/3.png"
-import Image4 from "./../Media/4.png"
-import Image5 from "./../Media/5.png"
-import Image6 from "./../Media/6.png"
 
 export const Main = () => {
     return (
         <>
 
-            <img src={Image2} alt="my profile1" className="bg-scroll" />
-            <img src={Image1} alt="my profile2" className="" />
-            <img src={Image3} alt="my profile3" className="" />
-            <img src={Image4} alt="my profile4" className="object-scale-down m-10 w-[80%] center" />
-            <img src={Image5} alt="my profile5" className="object-scale-down m-10 w-[80%] center" />
-            <img src={Image6} alt="my profile6" className="object-scale-down m-10 w-[80%] center" />
+            <img src={Image1} alt="my profile1" className="bg-scroll" />
+
             <div className='center'></div>
             <div className='flex'>
                 {/* dots */}
@@ -26,12 +18,7 @@ export const Main = () => {
                 <div>
                     {/* Hero */}
                     <div className='md:flex'>
-                        <img src={Image1} alt="my profile1" className="object-scale-down m-10 w-[80%] center" />
-                        <img src={Image2} alt="my profile2" className="object-scale-down m-10 w-[80%] center" />
-                        <img src={Image3} alt="my profile3" className="object-scale-down m-10 w-[80%] center" />
-                        <img src={Image4} alt="my profile4" className="object-scale-down m-10 w-[80%] center" />
-                        <img src={Image5} alt="my profile5" className="object-scale-down m-10 w-[80%] center" />
-                        <img src={Image6} alt="my profile6" className="object-scale-down m-10 w-[80%] center" />
+                        <img src={Image2} alt="my profile1" className="object-scale-down m-10 w-[80%] center" />
                         <div className='center'>
                             <h1>Electronics/Embedded Systems</h1>
                             <h1>Telecommunication Core Networks</h1>
@@ -41,7 +28,7 @@ export const Main = () => {
 
                     {/* content */}
                     <div className='m-10'>
-                        <p className="">Top-sellers in tsfsdhe past week!</p>
+                        <p className="">Top-sellers in tsfsdhe past week! My test</p>
                         <p className="">Top-sellers in tsfsdhe past week!</p>
                         <p className="">Top-sellers in tsfsdhe past week!</p>
                         <p className="">Top-sellers in tsfsdhe past week!</p>
