33a1a51ca270c4ba9f12609cab78be2609d257e9
Ali-vspace <37alirisalpur@gmail.com>
2022-12-25 14:09:08 +0500

    Yesd

--- src/MyComponents/Main.js
+++ src/MyComponents/Main.js
@@ -5,7 +5,7 @@ import Image2 from "./../Media/2.png"
 export const Main = () => {
     return (
         <>
-            <h1 className="text-red-500 text-center text-3xl">This website is under development by Muhammad Ali</h1>
+            <h1 className="text-red-500 text-center text-3xl">This website is under development by Muhammad Ali!</h1>
             <div className='md:flex'>
                 {/* <div className='center'>
                     <h1>Electronics/Embedded Systems</h1>
