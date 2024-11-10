13f74b5c0380807d85182adcebaf150a6a79a34f
Ali-vspace <37alirisalpur@gmail.com>
2024-11-10 23:17:28 +0100

    asdas

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import JYU from './CV.pdf';
 
 export const Intro = () => {
     return (
-        <div className="my-10 text-center"> {/* Adjust `my-20` for more or less space */}
-            Intro
+        <div className="flex flex-col items-center my-40 lg:items-end lg:mx-20">
+            <div className="w-full lg:w-3/4 text-center lg:text-left mb-6">
+                <p className="text-lg font-semibold text-red-600">
+                    The website is under development. Until the website is finalized, please see the CV below.
+                </p>
+            </div>
+            <div className="w-full lg:w-3/4">
+                <iframe
+                    src={JYU}
+                    title="CV"
+                    className="w-full h-screen"
+                />
+            </div>
         </div>
     );
 };
